768aeb991257c38ca955c83c781051b49b300d43
jb corbin <101614021+jbc2313@users.noreply.github.com>
2023-03-09 18:28:15 -0600

    selection function completed

--- main.c
+++ main.c
@@ -6,11 +6,32 @@ void listArray(int *pntr, int size) {
     for (x = 0; x < size; x++) {
         printf("array[%d]=%d\n", x, pntr[x]);
     };
+};
+
+void selection(int *pntr, int size) {
+    int x; 
+    for (x = 0; x < size - 1; x++) {
+        int min = x;
+        int i;
+        for (i = x + 1; i < size; i++) {
+            if (pntr[i] < pntr[min]) {
+                min = i;
+            };
+        };
+        // move the min to the correct position
+        int tmp = pntr[min];
+        pntr[min] = pntr[x];
+        pntr[x] = tmp;
+
+    };
 
 };
 
-
 int main() {
-
+    int array[] = {8, 11, 3, 5, 18, 4, 1, 13, 14};
+    int size = sizeof(array) / sizeof(array[0]);
+    listArray(array, size);
+    selection(array, size);
+    listArray(array, size);
     return 0;
 }
